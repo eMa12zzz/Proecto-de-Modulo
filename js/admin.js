5a28dd391e9efd41860f890ec9f25ec8eecd0408
eMa12zzz <20240230@ricaldone.edu.sv>
2025-06-24 13:21:41 -0600

    para ver si la damos

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,50 @@
 API_URL = 'https://685ac53c9f6ef9611157b82d.mockapi.io/emanuel_brandon/camas'
+
+const form = document.getElementById('persona-form');
+const nombreEl = document.getElementById('nombre');
+const telefonoEl = document.getElementById('telefono');
+const imagenFileEl = document.getElementById('imagen-file');
+const imagenUrlEl = document.getElementById('imagen-url');
+const idEl = document.getElementById('persona-id');
+const cancelBtn = document.getElementById('btn-cancel');
+const submitBtn = document.getElementById('btn-submit');
+const tbody = document.getElementById('personas-tbody');
+
+
+window,addEventListener('DOMContentLoaded', CargarProducto);
+
+async function CargarProducto() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    CargarTabla(data);
+}
+
+function CargarTabla(producto){
+    tbody.innerHTML = '';
+    producto.forEach(productos => {
+        tbody.innerHTML +=`
+        <tr>
+             <td><img src="${producto.imagen}" alt="Foto de ${producto.nombre}" /></td>
+             <td>${producto.nombre}</td>
+             <td>
+                <button onclick="CargarParaEditar('${producto.id}')>"Editar"</button>
+                <button onclick="BorrarPersona('${producto.id}')">Eliminar</button>
+             </td>
+        </tr>
+        `;
+    });
+}
+
+async function BorrarPersona(id) {
+    if(!confirm('¿Eliminar este producto de el registro?'))return;
+    await fetch(`${API_URL}/${id}`, {method: 'DELETE' });
+    CargarProducto();
+    alert("El producto fue eliminado");
+}
+
+async function CargarParaEditar(id) {
+    const res = await fetch(`${API_URL}/$${id}`);
+    const p = await res.json();
+
+
+}
--- js/inicio.js
+++ js/inicio.js
@@ -1,1 +1,34 @@
 API_URL = 'https://685ac53c9f6ef9611157b82d.mockapi.io/emanuel_brandon/camas'
+
+const container = document.getElementById('cards-container');
+
+async function CargarProducto(producto) {
+    try{
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        CargarTarjetas(data);
+    } catch(err){
+        console.error('Error al tratar de cargar datos', err);
+        container.innerHTML = '<p>Error al cargar los productos</p>';
+    }
+}
+
+function CargarTarjetas(producto){
+    container.innerHTML = '';
+    if(!producto.length){
+        container.innerHTML = '<p>No hay productos disponibles, intentelo denuevo mas tarde</p>';
+        return;
+    }
+
+    producto.forEach(p => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+            <img src="${p.imagen}" alt="Foto de ${p.nombre}">
+            <h2>${p.nombre}</h2>
+        `;
+        container.appendChild(card);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', CargarProducto);
